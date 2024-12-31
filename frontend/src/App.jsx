a014c58c6115009e8912542751e3234de8ed6f51
manosanti <luccassantiago.contato@gmail.com>
2024-12-31 13:10:25 -0300

    feat: navigation bar (w/ functionalities)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import HeroPage from './components/HeroPage'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className='bg-zinc-950'>
+      <Navbar />
+      {/* <HeroPage /> */}
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState } from "react";
+// Images
+import brazilFlag from "../assets/icons/brazil-flag.png";
+import usaFlag from "../assets/icons/united-states-flag.png";
+
+function Navbar() {
+  const [currentLanguage, setCurrentLanguage] = useState("pt-br");
+
+  const portfolioLogo = 'manosanti.dev'
+
+  const language = [
+    {
+      language: "en-us",
+      links: [
+        { name: "About", link: "#about" },
+        { name: "Work", link: "#work" },
+        { name: "Testimonials", link: "#testimonials" },
+        { name: "Contact", link: "#contact" },
+      ],
+      download: { text: "Download CV", link: "#download-cv" },
+      flag: usaFlag,
+    },
+    {
+      language: "pt-br",
+      links: [
+        { name: "Sobre", link: "#sobre" },
+        { name: "Trabalhos", link: "#trabalhos" },
+        { name: "Depoimentos", link: "#depoimentos" },
+        { name: "Contato", link: "#contato" },
+      ],
+      download: { text: "Baixar CV", link: "#baixar-cv" },
+      flag: brazilFlag,
+    },
+  ];
+
+  const currentLang = language.find((lang) => lang.language === currentLanguage);
+
+  const toggleLanguage = () => {
+    setCurrentLanguage(currentLanguage === "pt-br" ? "en-us" : "pt-br");
+  };
+
+  return (
+    <>
+      <div className="w-screen flex items-center max-w-7xl mx-auto justify-between h-16 text-white">
+        <div>
+          <a href="#!" className="font-bold text-2xl">{portfolioLogo}</a>
+        </div>
+        <div className="flex list-none items-center">
+          <ul className="flex gap-6">
+            {currentLang.links.map((item) => (
+              <li key={item.name} className="hover:text-blue-600 transition duration-300">
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+          <ul className="flex mx-12 gap-2">
+            {language.map((lang) => (
+              <li key={lang.language}>
+                <button onClick={() => setCurrentLanguage(lang.language)}>
+                  <img src={lang.flag} alt="" width={28} />
+                </button>
+              </li>
+            ))}
+          </ul>
+          <ul className="w-28">
+            <li className="text-black bg-white p-1 font-medium rounded-md min-w-8">
+              <a href={currentLang.download.link} target="_blank" rel="noopener noreferrer">
+                {currentLang.download.text}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
